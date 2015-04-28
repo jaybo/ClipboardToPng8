aa8b8597d9a20ef72600138b9290ac5f0282343e
Jay Borseth <jaybo@nomadelectronics.com>
2015-04-28 08:02:02 -0700

    Initial checkin

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace ClipboardToPng8
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (Clipboard.ContainsImage()) {
+                var d = DateTime.Now;
+                //var tname = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                //    "ClipBoard.temp.png");
+                var fname = Path.Combine( Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) , 
+                    "ClipBoard." + d.ToString("yyyy.MM.dd.HH.mm.ss") + ".png");
+                
+                var img = Clipboard.GetImage();
+                var bmp = new Bitmap(img);
+                //img.Save(fname);
+
+                var image = new  ImageMagick.MagickImage(bmp);
+                //image.ColorType = ImageMagick.ColorType.Palette;
+                //image.Resize(new ImageMagick.MagickImage.Geometry("50%"));
+                
+                image.Write("png8:" + fname);
+
+                Console.WriteLine("Saved image as:" + Environment.NewLine + fname);
+            }
+            else
+            {
+                Console.WriteLine("Error: no image in clipboard");
+            }
+            Console.ReadKey();
+        }
+    }
+}
